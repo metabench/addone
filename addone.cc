64a4551e6e04b900062e013c2f1f8bc05f4b5542
James Vickers <james@metabench.com>
2016-08-23 22:27:08 +0100

    Adds 1 to an int. A C++ addon. Does not use nan.

---
+++ addone.cc
@@ -0,0 +1,27 @@
+// hello.cc
+#include <node.h>
+#include "addone_inner.cc"
+
+namespace demo {
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Number;
+using v8::Value;
+
+void Addone(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  int res = addone(args[0]->NumberValue());
+  args.GetReturnValue().Set(Number::New(isolate, res));
+}
+
+void init(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "addone", Addone);
+}
+
+NODE_MODULE(addone, init)
+
+}  // namespace demo
---
+++ binding.gyp
@@ -0,0 +1,8 @@
+{
+    "targets": [
+        {
+            "target_name": "addone",
+            "sources": [ "addone.cc"]
+        }
+    ],
+}
