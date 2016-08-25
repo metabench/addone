f7a10acfa17098742b6538d47b7c7ac1e02c965d
James Vickers <james@metabench.com>
2016-08-25 12:25:57 +0100

    Writing code to process Typed Arrays (Float64Array). Don't yet know how to create V8 typed arrays to be returned.

--- addone.cc
+++ addone.cc
@@ -1,8 +1,11 @@
 // hello.cc
 #include <node.h>
 #include "addone_inner.cc"
+#include <iostream>
 
-namespace demo {
+using namespace std;
+
+namespace simple {
 
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
@@ -11,6 +14,7 @@ using v8::Object;
 using v8::String;
 using v8::Number;
 using v8::Value;
+using v8::Handle;
 
 void Addone(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
@@ -21,10 +25,38 @@ void Addone(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(Number::New(isolate, res));
 }
 
+void Addone_TA(const FunctionCallbackInfo<Value>& args) {
+
+  const double *x = reinterpret_cast<double*>(args[0].As<v8::Float64Array>()->Buffer()->GetContents().Data());
+  int l = sizeof(*x);
+  cout << "Length of array = " << (l) << endl;
+
+  double* res = new double[l];
+
+  for (int c = 0; c < l; c++) {
+    res[c] = x[c] + 1;
+  }
+
+  //v8::Handle<v8::JSTypedArray> typed_array;
+
+  // Outputting a new typed array...
+
+  //args.GetReturnValue().Set(
+  //		v8::Float64Array::New(args.GetIsolate(), res)
+  //	);
+
+
+
+
+
+
+}
+
 void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "addone", Addone);
+  NODE_SET_METHOD(exports, "addone_ta", Addone_TA);
 }
 
 NODE_MODULE(addone, init)
 
-}  // namespace demo
+}  // namespace simple
--- addone.js
+++ addone.js
@@ -5,3 +5,10 @@ var addone = require('./build/Release/addone');
 
 var res = addone.addone(1.1);
 console.log('res', res);
+
+var ta = new Float64Array(8);
+ta.fill(1);
+
+console.log('pre addone_ta');
+var res2 = addone.addone_ta(ta);
+console.log('res2', res2);
