446c161103e657a2f3a741a85330cb14be36f8af
James Vickers <james@metabench.com>
2016-09-07 22:12:51 +0100

    Tidied the code, improved example.

--- addone.cc
+++ addone.cc
@@ -20,38 +20,65 @@ using v8::ArrayBuffer;
 void Addone(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
 
-  //
-
   double res = addone(args[0]->NumberValue());
   args.GetReturnValue().Set(Number::New(isolate, res));
 }
 
 void Addone_TA_F64(const FunctionCallbackInfo<Value>& args) {
+  // Get the input values, accounting for subarrays
   v8::Local<v8::Float64Array> fa_input = args[0].As<v8::Float64Array>();
   int l = fa_input -> ByteLength() / sizeof(double);
+  void *data = fa_input->Buffer()->GetContents().Data();
+  size_t offset = fa_input->ByteOffset();
+  double *x = reinterpret_cast<double*>(static_cast<unsigned char*>(data) + offset);
+  double* res = new double[l];
+
+  // Inner processing
+  for (int c = 0; c < l; c++) {
+    res[c] = addone(x[c]);
+  }
+
+  // Output a new typed array
+  Local<ArrayBuffer> ab = v8::ArrayBuffer::New (args.GetIsolate(), res, l * sizeof(double));
+	delete[] data;
+  v8::Local<v8::Float64Array> f64a_res = v8::Float64Array::New(ab, 0, l);
+  args.GetReturnValue().Set(f64a_res);
+}
+
 /*
+void _Addone_TA_F64(const FunctionCallbackInfo<Value>& args) {
+  v8::Local<v8::Float64Array> fa_input = args[0].As<v8::Float64Array>();
+  int l = fa_input -> ByteLength() / sizeof(double);
+
   void *data = (fa_input->Buffer()->GetContents().Data());
   size_t offset = fa_input->ByteOffset();
   unsigned char* data_offset = static_cast<unsigned char*>(data + offset);
   const double *x = reinterpret_cast<double*>(data_offset);
-*/
 
-/*
+
+
 
   void *data = fa_input->Buffer()->GetContents().Data();
   size_t offset = fa_input->ByteOffset();
   unsigned char* data_offset = static_cast<unsigned char*>(data + offset);
   double *x = reinterpret_cast<double*>(data_offset);
-  */
+
 
   //v8::Local<v8::Float64Array> array = argument.As<v8::Float64Array>();
+
+
   void *data = fa_input->Buffer()->GetContents().Data();
-
   size_t offset = fa_input->ByteOffset();
-  size_t data_ptr = reinterpret_cast<size_t>(data);
-  data_ptr += offset;
+  //size_t data_ptr = reinterpret_cast<size_t>(data);
+  //data_ptr += offset;
+  //double *x = reinterpret_cast<double*>(data_ptr);
 
-  double *x = reinterpret_cast<double*>(data_ptr);
+
+  //unsigned char *data_offset = static_cast<unsigned char*>(data) + offset;
+  //double *x = reinterpret_cast<double*>(data_offset);
+
+  double *x = reinterpret_cast<double*>(static_cast<unsigned char*>(data) + offset);
+
 
   //const double *x = reinterpret_cast<double*>
   //const double *x = reinterpret_cast<double*>(fa_input -> Buffer() -> GetContents().Data());
@@ -73,7 +100,7 @@ void Addone_TA_F64(const FunctionCallbackInfo<Value>& args) {
   v8::Local<v8::Float64Array> f64a_res = v8::Float64Array::New(ab, 0, l);
   args.GetReturnValue().Set(f64a_res);
 }
-
+*/
 void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "addone", Addone);
   NODE_SET_METHOD(exports, "addone_ta_f64", Addone_TA_F64);
--- addone.js
+++ addone.js
@@ -9,7 +9,7 @@ console.log('res', res);
 var ta = new Float64Array(4);
 ta.fill(1);
 
-var ta2 = new Float64Array([1,20,300]).subarray(1,2);
+var ta2 = new Float64Array([1,20,300, 4000]).subarray(1,3);
 console.log('ta2', ta2);
 
 var res2 = addone.addone_ta_f64(ta2);
